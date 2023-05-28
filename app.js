5fb1c3774f53a32f8ddde9f3b7caad51ba291d8b
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-05-28 22:59:15 +0100

    cleanup code

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const mongoose = require('mongoose');
-var cors = require('cors');
 const passport = require('passport');
-// require('dotenv').config();
 
 const sliderRouter = require('./routes/sliderRouter');
 const blogRouter = require('./routes/blogRouter');
@@ -76,27 +74,6 @@ app.use('/pupup', pupupRouter);
 app.use('/images', imageRouter);
 app.use('/surveys', surveyRouter);
 app.use('/feedbacks', feedbackRouter);
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
  app.use(express.static('./client/build'));
   app.get('*', (req, res) => {
